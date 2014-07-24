ad96e5fe5a0d6045eeed55d6c4593569d5b34150
Jerry Huo <jerry.huo@tbwaworld.com>
2014-07-24 16:17:18 +0800

    add github data

--- app/config/packages/artdarek/oauth-4-laravel/config.php
+++ app/config/packages/artdarek/oauth-4-laravel/config.php
@@ -21,11 +21,20 @@ return array(
 		/**
 		 * Twitter
 		 */
-        'Twitter' => array(
-            'client_id'     => 'krdI5H9QUqTHGeTTPUiPSK4LW',
-            'client_secret' => 'Z1QDJeImqXjre8OsuxkVqtPIEjXz3l69E3E7s89tDGjtBzFoBN',
-            // 'scope'         => array(),
-        ),		
+    'Twitter' => array(
+        'client_id'     => 'krdI5H9QUqTHGeTTPUiPSK4LW',
+        'client_secret' => 'Z1QDJeImqXjre8OsuxkVqtPIEjXz3l69E3E7s89tDGjtBzFoBN',
+        // 'scope'         => array(),
+    ),		
+
+    /**
+     * Github
+     */
+    'Github' => array(
+        'client_id'     => '69090fd5c61bfc878245',
+        'client_secret' => 'a26014b5e0ade38be4759561af8b7419d15fa64a',
+        'scope'         => array("user:email"),
+    ),
 
 	)
 
